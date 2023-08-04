596420d60f466a33514ec9a40357026cf5500973
Jose <josmapizarro.99@gmail.com>
2023-08-04 14:19:01 -0400

    Bookmarkelet script and detail image view

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls')),
+    path('images/',include('images.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='images_created',on_delete=models.CASCADE)
@@ -11,7 +12,7 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='images_liked',blank=True)
-    
+
 
     class Meta:
         indexes = [
@@ -26,3 +27,8 @@ class Image(models.Model):
             self.slug = slugify(self.title)
         super().save(*args,**kwargs)
     
+    def get_absolute_url(self):
+        return reverse('images:detail',args=[
+            self.id,
+            self.slug
+        ])
